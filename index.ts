71ec4ba33f9632911f7c2e3d4962ccd11de92599
@suriv27121994 <suriv2712194@gmail.com>
2021-12-29 14:19:17 +0800

    add file index.ts

---
+++ index.ts
@@ -0,0 +1,7 @@
+// Accessing the property 'toLowerCase'
+// on 'message' and then calling it
+const msg = "Hello World!";
+msg.toLowerCase(); //msg is message
+// Calling 'message'
+//msg();
+console.log(msg);
